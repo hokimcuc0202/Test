56cdf1988ec7f66850922068eb97bd1336f1e94b
hokimcuc0202 <kimcuc0202@gmail.com>
2018-04-09 10:51:52 +0700

    Create getRecord.js

---
+++ getRecord.js
@@ -0,0 +1,16 @@
+function getRecord() {
+    var params = '?app=20&id=100';
+    var url = 'https://{subdomain}.kintone.com/k/v1/record.json' + params;
+
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', url);
+    xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
+    xhr.onload = function() {
+        if (xhr.status === 200) {
+            JSON.parse(xhr.responseText);
+        } else {
+            JSON.parse(xhr.responseText);
+        }
+    };
+    xhr.send();
+}
